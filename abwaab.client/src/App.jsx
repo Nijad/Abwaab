4e09087efaf35bfae65424c3d211d313c6e9596a
Husam Habbal <husam-develop@outlook.com>
2026-07-27 18:30:23 +0300

    working on app structure

--- abwaab.client/src/App.jsx
+++ abwaab.client/src/App.jsx
@@ -1,51 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AppProviders from "./context/AppProviders";
+import { AppRouter } from "./routes/AppRouter";
 
 function App() {
-    const [forecasts, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
+  return (
+    <AppProviders>
+      <AppRouter />
+      <div>
+        <h1 id="" className="text-blue-900 text-3xl font-bold underline">
+          ABWAAB For Real Estates Trading
+        </h1>
+        <h4 className="text-3xl">COMING SOON!</h4>
+      </div>
+    </AppProviders>
+  );
 }
 
 export default App;
---
+++ abwaab.client/src/hooks/useAxiosPrivate.js
@@ -0,0 +1,44 @@
+import { axiosPrivate } from "../components/APIs/axios";
+import { useEffect } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuth from "./useAuth";
+import { error } from "jquery";
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        // if (!config.headers["Authorization"]) {
+        //   config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+        // }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+    const responsIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+        if (
+          (error?.response?.status === 403 || error?.response.status === 401) &&
+          !prevRequest?.sent
+        ) {
+          prevRequest.sent = true;
+          const newAccessTokent = await refresh();
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessTokent}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      axiosPrivate.interceptors.response.eject(responsIntercept);
+      axiosPrivate.interceptors.response.eject(requestIntercept);
+    };
+  }, [auth, refresh]);
+  return axiosPrivate;
+};
+export default useAxiosPrivate;
