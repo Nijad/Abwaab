62a027b79b6a6c8baa897d9976af3f247cea59d4
Nijad <nijad-k@live.com>
2026-08-29 19:39:27 +0300

    order time slots

---
+++ Abwaab.Application/Common/Mappings/DayOfWeekMapping.cs
@@ -0,0 +1,33 @@
+﻿namespace Abwaab.Application.Common.Mappings;
+
+public static class DayOfWeekMapping
+{
+    public static string Map(DayOfWeek day)
+    {
+        return day switch
+        {
+            DayOfWeek.Sunday => "الأحد",
+            DayOfWeek.Monday => "الاثنين",
+            DayOfWeek.Tuesday => "الثلاثاء",
+            DayOfWeek.Wednesday => "الأربعاء",
+            DayOfWeek.Thursday => "الخميس",
+            DayOfWeek.Friday => "الجمعة",
+            DayOfWeek.Saturday => "السبت",
+            _ => ""
+        };
+    }
+    public static string Map(int day)
+    {
+        return day switch
+        {
+            0 => "الأحد",
+            1 => "الاثنين",
+            2 => "الثلاثاء",
+            3 => "الأربعاء",
+            4 => "الخميس",
+            5 => "الجمعة",
+            6 => "السبت",
+            _ => ""
+        };
+    }
+}
--- Abwaab.Application/Common/Mappings/UserPlanStatesMapping.cs
+++ Abwaab.Application/Common/Mappings/UserPlanStatesMapping.cs
@@ -1,35 +1,33 @@
 ﻿using Abwaab.Application.Common.Enums;
+namespace Abwaab.Application.Common.Mappings;
 
-namespace Abwaab.Application.Common.Mappings
+public static class UserPlanStatesMapping
 {
-    public static class UserPlanStatesMapping
+    public static string Map(UserPlanStatesEnum state)
     {
-        public static string Map(UserPlanStatesEnum state)
+        return state switch
         {
-            return state switch
-            {
-                UserPlanStatesEnum.Pending => "بالانتظار",
-                UserPlanStatesEnum.Active => "فعال",
-                UserPlanStatesEnum.Working => "يعمل",
-                UserPlanStatesEnum.Expiered => "منتهي الصلاحية",
-                UserPlanStatesEnum.Canceled => "ملغى",
-                _ => ""
-            };
-        }
+            UserPlanStatesEnum.Pending => "بالانتظار",
+            UserPlanStatesEnum.Active => "فعال",
+            UserPlanStatesEnum.Working => "يعمل",
+            UserPlanStatesEnum.Expiered => "منتهي الصلاحية",
+            UserPlanStatesEnum.Canceled => "ملغى",
+            _ => ""
+        };
+    }
 
-        public static string Map(string state)
-        {
-            if(state == UserPlanStatesEnum.Pending.ToString())
-                return Map(UserPlanStatesEnum.Pending);
-            if(state == UserPlanStatesEnum.Active.ToString())
-                return Map(UserPlanStatesEnum.Active);
-            if(state == UserPlanStatesEnum.Working.ToString())
-                return Map(UserPlanStatesEnum.Working);
-            if(state == UserPlanStatesEnum.Expiered.ToString())
-                return Map(UserPlanStatesEnum.Expiered);
-            if(state == UserPlanStatesEnum.Canceled.ToString())
-                return Map(UserPlanStatesEnum.Canceled);
-            return "";
-        }
+    public static string Map(string state)
+    {
+        if (state == UserPlanStatesEnum.Pending.ToString())
+            return Map(UserPlanStatesEnum.Pending);
+        if (state == UserPlanStatesEnum.Active.ToString())
+            return Map(UserPlanStatesEnum.Active);
+        if (state == UserPlanStatesEnum.Working.ToString())
+            return Map(UserPlanStatesEnum.Working);
+        if (state == UserPlanStatesEnum.Expiered.ToString())
+            return Map(UserPlanStatesEnum.Expiered);
+        if (state == UserPlanStatesEnum.Canceled.ToString())
+            return Map(UserPlanStatesEnum.Canceled);
+        return "";
     }
 }
